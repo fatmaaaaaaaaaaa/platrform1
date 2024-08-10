d55fdea0a056fd1c9a581a8fd9a6ca7163f51b5e
AydiChayma <chaymaaydi33@gmail.com>
2024-08-10 20:05:05 +0100

    yes

--- platform1/src/App.jsx
+++ platform1/src/App.jsx
@@ -3,17 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Loginparent from './LoginParent/Loginparent'
 import Loginteacher from './LoginTeacher/Loginteacher'
 import WhoAreYou from './WhoAreYou/WhoAreYou'
-
+import LandingPage from './LandingPqge/Body';
+import Header from './LandingPqge/Header';
 function App() {
 
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<WhoAreYou />} />
-          <Route path="/Loginteacher" element={<Loginteacher />} />
-          <Route path="/Loginparent" element={<Loginparent />} />
-        </Routes>
+        <div className="app-container">
+          <Header />
+          <div className="content-container">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/WhoAreYou" element={<WhoAreYou />} />
+              <Route path="/Loginteacher" element={<Loginteacher />} />
+              <Route path="/Loginparent" element={<Loginparent />} />
+            </Routes>
+          </div>
+        </div>
       </Router>
     </>
   )
